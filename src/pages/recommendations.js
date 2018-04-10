42567b18efc4b46bd812d183ca88a651dc1ed466
Lauren Stephenson <compscilauren@gmail.com>
2018-04-09 22:50:32 -0500

    Fixed picture import

--- src/pages/recommendations.js
+++ src/pages/recommendations.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './recommendations.css';
 import TomPagano from './tompagano.jpg';
-import RecommendationPerson from './recommendationperson.jpg';
+import RecommendationPerson from './recommendationperson.png';
 
 const Recommendations = () => (
   <div>
