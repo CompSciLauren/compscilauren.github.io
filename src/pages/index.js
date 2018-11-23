bf4b418172afb9a0dd61485a2240205e37dbb514
Lauren Stephenson <compscilauren@gmail.com>
2018-11-23 15:37:40 -0600

    Rearranged sections on home page and made some sections easier to look at and understand visually

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { withPrefix } from 'gatsby-link';
 
 import './about.css';
 import PortfolioListing from '../components/PortfolioListing/PortfolioListing';
-import lauren_holding_cat from './images/contact/lauren_holding_cat.jpg';
 import christy_mcward from './images/recommendations/christy_mcward.jpg';
 import tom_pagano from './images/recommendations/tom_pagano.jpg';
 import circuit_maker from './images/portfolio/circuit_maker.png';
@@ -70,131 +69,139 @@ const About = () => (
       </div>
     </div>
     <h3 className="text-center">View Resume</h3>
-
     <br />
     <br />
     <div className="darker-background">
       <hr />
-      <h1>Recommendations</h1>
-      <div className="christy-mcward-picture-container">
-        <img src={christy_mcward} alt="Christy Mcward portrait picture." />
+      <h1 className="text-center">Recommendations</h1>
+      <div className="center-div">
+        <div className="row">
+          <div className="column">
+            <div className="christy-mcward-picture-container">
+              <img
+                src={christy_mcward}
+                alt="Christy Mcward portrait picture."
+              />
+            </div>
+            <h3 className="left">
+              Christy McWard, Director of Communications & Advancement at
+              University of Kansas
+            </h3>
+            <h4>April 2018</h4>
+            <p>
+              “Lauren is exceptional in every one of the many roles she plays at
+              Johnson County Community College. As a student, she works hard and
+              excels in her classes while building strong relationships with
+              faculty and classmates. As a campus leader, she is engaged in
+              several groups, not only as a member, but often as an officer, a
+              co-founder, an event planner, or a public speaker on behalf of the
+              groups she belongs to. As an employee of JCCC's Collaboration
+              Center, the CoLab, where I was her supervisor, she was a shining
+              example of what it means to be a committed, responsible and
+              enthusiastic colleague. And finally, she is a delightful person
+              through and through. Her kindness, compassion and curiosity about
+              the world are contagious among all who have the good fortune to
+              work with her."
+            </p>
+          </div>
+          <div className="column">
+            <div className="tom-pagano-picture-container">
+              <img src={tom_pagano} alt="Tom Pagano portrait picture." />
+            </div>
+            <h3 className="left">
+              Tom Pagano, VP/CIO at Johnson County Community College
+            </h3>
+            <h4>December 2017</h4>
+            <p>
+              “I met Lauren through the JCCC Student Computer Club. After an
+              insightful and vibrant first meeting, I was impressed with her
+              desire to enhance the Computer Club experience. As the President
+              of the Club, Lauren, worked with the Faculty Facilitator to
+              -incorporate me into the meetings. It has been a very rewarding
+              experience for me thus far and the feedback from the group has
+              been positive. Lauren has visited with me and on a number of
+              occasions to talk about career planning, course selection and how
+              to integrate with local IT professional organizations. Lauren is
+              well poised in her education to make innovative contributions to
+              the IT Industry, the local organizations she will become involved
+              with and the way we use technology to make our lives better. I
+              look forward to the ongoing interaction with Lauren as she moves
+              on to future success.”
+            </p>
+          </div>
+        </div>
       </div>
-      <h3 className="left">
-        Christy McWard, Director of Communications & Advancement at University
-        of Kansas
-      </h3>
-      <h4>April 2018</h4>
-      <p>
-        “Lauren is exceptional in every one of the many roles she plays at
-        Johnson County Community College. As a student, she works hard and
-        excels in her classes while building strong relationships with faculty
-        and classmates. As a campus leader, she is engaged in several groups,
-        not only as a member, but often as an officer, a co-founder, an event
-        planner, or a public speaker on behalf of the groups she belongs to. As
-        an employee of JCCC's Collaboration Center, the CoLab, where I was her
-        supervisor, she was a shining example of what it means to be a
-        committed, responsible and enthusiastic colleague. And finally, she is a
-        delightful person through and through. Her kindness, compassion and
-        curiosity about the world are contagious among all who have the good
-        fortune to work with her."
-      </p>
-      <hr />
-      <br />
-      <div className="tom-pagano-picture-container">
-        <img src={tom_pagano} alt="Tom Pagano portrait picture." />
-      </div>
-      <h3 className="left">
-        Tom Pagano, VP/CIO at Johnson County Community College
-      </h3>
-      <h4>December 2017</h4>
-      <p>
-        “I met Lauren through the JCCC Student Computer Club. After an
-        insightful and vibrant first meeting, I was impressed with her desire to
-        enhance the Computer Club experience. As the President of the Club,
-        Lauren, worked with the Faculty Facilitator to incorporate me into the
-        meetings. It has been a very rewarding experience for me thus far and
-        the feedback from the group has been positive. Lauren has visited with
-        me and on a number of occasions to talk about career planning, course
-        selection and how to integrate with local IT professional organizations.
-        Lauren is well poised in her education to make innovative contributions
-        to the IT Industry, the local organizations she will become involved
-        with and the way we use technology to make our lives better. I look
-        forward to the ongoing interaction with Lauren as she moves on to future
-        success.”
-      </p>
     </div>
-    <hr />
     <br />
-    <h2>News Stories</h2>
-    <p>
-      <a
-        href="http://www2.ljworld.com/news/2018/jun/05/got-daughter-interested-computers-lawrence-now-has/"
-        target="_blank"
-      >
-        Have a daughter interested in computers? Lawrence now has a ‘Girls Who
-        Code’ club
-      </a>
+    <div className="contact">
+      <h1 className="text-center">Contact Me</h1>
+      <p>
+        I am always up for a challenge. Are you looking to collaborate on a
+        project? In need of a guest speaker or volunteer for a worthwhile cause?
+        Perhaps you just want to ask me a question. If you have anything to say,
+        I'm always open to listening! Please feel free to contact me at any
+        time.
+      </p>
+      <div className="custom-image-adjust">
+        <i className="fa fa-envelope-o"> compscilauren@gmail.com</i>
+      </div>
+      <div className="custom-image-adjust">
+        <a href="https://twitter.com/CompSciLauren" target="_blank">
+          <i className="fa fa-twitter" />
+        </a>
+      </div>
+      <div className="custom-image-adjust">
+        <a href="https://github.com/CompSciLauren" target="_blank">
+          <i className="fa fa-github" />
+        </a>
+      </div>
+      <div className="custom-image-adjust">
+        <a href="https://www.linkedin.com/in/compscilauren/" target="_blank">
+          <i className="fa fa-linkedin" />
+        </a>
+      </div>
+    </div>
+    <div className="news-stories">
       <br />
+      <hr />
+      <h2>News Stories</h2>
+      <p>
+        <a
+          href="http://www2.ljworld.com/news/2018/jun/05/got-daughter-interested-computers-lawrence-now-has/"
+          target="_blank"
+        >
+          Have a daughter interested in computers? Lawrence now has a ‘Girls Who
+          Code’ club
+        </a>
+        <br />
+        <a
+          href="http://blogs.jccc.edu/campusledger/2017/11/07/some-students-so-involved-others-not-so-much/"
+          target="_blank"
+        >
+          Some students so involved, others not so much
+        </a>
+        <br />
+        <a
+          href="https://blogs.jccc.edu/campusledger/2017/10/11/honors-program-holds-used-book-sale-to-support-student-scholarships/"
+          target="_blank"
+        >
+          Honors program holds used book sale to support student scholarships
+        </a>
+        <br />
+        <a
+          href="http://www.gardneredge.com/featured/gehs-students-initiated-into-national-technical-honor-society/"
+          target="_blank"
+        >
+          GEHS students initiated into National Technical Honor Society
+        </a>
+      </p>
       <a
-        href="http://blogs.jccc.edu/campusledger/2017/11/07/some-students-so-involved-others-not-so-much/"
-        target="_blank"
+        className="twitter-timeline"
+        data-height="500"
+        data-link-color="#981CEB"
+        href="https://twitter.com/CompSciLauren?ref_src=twsrc%5Etfw"
       >
-        Some students so involved, others not so much
-      </a>
-      <br />
-      <a
-        href="https://blogs.jccc.edu/campusledger/2017/10/11/honors-program-holds-used-book-sale-to-support-student-scholarships/"
-        target="_blank"
-      >
-        Honors program holds used book sale to support student scholarships
-      </a>
-      <br />
-      <a
-        href="http://www.gardneredge.com/featured/gehs-students-initiated-into-national-technical-honor-society/"
-        target="_blank"
-      >
-        GEHS students initiated into National Technical Honor Society
-      </a>
-    </p>
-    <a
-      className="twitter-timeline"
-      data-height="500"
-      data-link-color="#981CEB"
-      href="https://twitter.com/CompSciLauren?ref_src=twsrc%5Etfw"
-    >
-      Tweets by CompSciLauren
-    </a>
-    <div className="custom-image-adjust">
-      <img
-        className="img-responsive"
-        src={lauren_holding_cat}
-        alt="Lauren holding an orange cat."
-      />
-    </div>
-    <h1 className="text-center">Contact Lauren Stephenson</h1>
-    <p>
-      I am always up for a challenge. Are you looking to collaborate on a
-      project? In need of a guest speaker or volunteer for a worthwhile cause?
-      Perhaps you just want to ask me a question. If you have anything to say,
-      I'm always open to listening! Please feel free to contact me at any time.
-    </p>
-    <hr />
-    <div className="custom-image-adjust">
-      <i className="fa fa-envelope-o"> compscilauren@gmail.com</i>
-    </div>
-    <div className="custom-image-adjust">
-      <a href="https://twitter.com/CompSciLauren" target="_blank">
-        <i className="fa fa-twitter" />
-      </a>
-    </div>
-    <div className="custom-image-adjust">
-      <a href="https://github.com/CompSciLauren" target="_blank">
-        <i className="fa fa-github" />
-      </a>
-    </div>
-    <div className="custom-image-adjust">
-      <a href="https://www.linkedin.com/in/compscilauren/" target="_blank">
-        <i className="fa fa-linkedin" />
+        Tweets by CompSciLauren
       </a>
     </div>
   </div>
