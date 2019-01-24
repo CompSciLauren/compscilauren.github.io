19906b93e734a769616dcfee656dfcd544a3d27a
Lauren Stephenson <compscilauren@gmail.com>
2019-01-23 20:42:44 -0600

    Fixed contact me section of home page so it fits better and more centered on page

--- src/pages/index.js
+++ src/pages/index.js
@@ -170,32 +170,43 @@ const About = () => (
     <br />
     <div className="contact">
       <h1 className="text-center">Contact Me</h1>
-      <p>
-        I am always up for a challenge. Are you looking to collaborate on a
-        project? In need of a guest speaker or volunteer for a worthwhile cause?
-        Perhaps you just want to ask me a question. If you have anything to say,
-        I'm always open to listening! Please feel free to contact me at any
-        time.
-      </p>
-      <div className="custom-image-adjust">
-        <i className="fa fa-envelope-o"> compscilauren@gmail.com</i>
-      </div>
-      <div className="custom-image-adjust">
-        <a href="https://twitter.com/CompSciLauren" target="_blank">
-          <i className="fa fa-twitter" />
-        </a>
-      </div>
-      <div className="custom-image-adjust">
-        <a href="https://github.com/CompSciLauren" target="_blank">
-          <i className="fa fa-github" />
-        </a>
-      </div>
-      <div className="custom-image-adjust">
-        <a href="https://www.linkedin.com/in/compscilauren/" target="_blank">
-          <i className="fa fa-linkedin" />
-        </a>
+
+      <div className="center-div">
+        <div className="row">
+          <p>
+            I am always up for a challenge. Are you looking to collaborate on a
+            project? In need of a guest speaker or volunteer for a worthwhile
+            cause? Perhaps you just want to ask me a question. If you have
+            anything to say, I'm always open to listening! Please feel free to
+            contact me at any time.
+          </p>
+        </div>
+        <div className="row">
+          <div className="custom-image-adjust">
+            <i className="fa fa-envelope-o"> compscilauren@gmail.com</i>
+          </div>
+          <div className="custom-image-adjust">
+            <a href="https://twitter.com/CompSciLauren" target="_blank">
+              <i className="fa fa-twitter" />
+            </a>
+          </div>
+          <div className="custom-image-adjust">
+            <a href="https://github.com/CompSciLauren" target="_blank">
+              <i className="fa fa-github" />
+            </a>
+          </div>
+          <div className="custom-image-adjust">
+            <a
+              href="https://www.linkedin.com/in/compscilauren/"
+              target="_blank"
+            >
+              <i className="fa fa-linkedin" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
+
     <div className="center-div">
       <div className="row">
         <div className="column footer-piece">
