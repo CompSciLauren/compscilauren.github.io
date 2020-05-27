a8940428f8ea5f86b78fc4049486ac1c65c55915
Lauren Stephenson <CompSciLauren@gmail.com>
2020-05-26 21:04:40 -0500

    Remove unused a href tag

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Experience = (props) => {
   return (
-    <a href={props.link} target="_blank">
-      <div class="experience-grid-item">
-        <h2>{props.company}</h2>
-        <h3>{props.job}</h3>
-        <p>
-          {props.startDate} - {props.endDate}
-        </p>
-        {props.accomplishments}
-      </div>
-    </a>
+    <div class="experience-grid-item">
+      <h2>{props.company}</h2>
+      <h3>{props.job}</h3>
+      <p>
+        {props.startDate} - {props.endDate}
+      </p>
+      {props.accomplishments}
+    </div>
   );
 };
 
--- src/components/Involvement.jsx
+++ src/components/Involvement.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 const Involvement = (props) => {
   return (
-    <a href={props.link} target="_blank">
-      <div class="involvement-grid-item">
-        <h2>{props.company}</h2>
-        <h3>{props.job}</h3>
-        <p>
-          {props.startDate} - {props.endDate}
-        </p>
-      </div>
-    </a>
+    <div class="involvement-grid-item">
+      <h2>{props.company}</h2>
+      <h3>{props.job}</h3>
+      <p>
+        {props.startDate} - {props.endDate}
+      </p>
+    </div>
   );
 };
 
