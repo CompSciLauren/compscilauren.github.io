6da96dff34b9208a0167462a7505dfc739cfe29c
Lauren Stephenson <CompSciLauren@gmail.com>
2020-05-26 21:17:08 -0500

    Improve experience design

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,13 +2,19 @@ import React from "react";
 
 const Experience = (props) => {
   return (
-    <div class="experience-grid-item">
-      <h2>{props.company}</h2>
-      <h3>{props.job}</h3>
-      <p>
-        {props.startDate} - {props.endDate}
-      </p>
-      {props.accomplishments}
+    <div className="experience-grid-item">
+      <div className="experience-grid-subitem">
+        <h2>{props.company}</h2>
+      </div>
+      <div className="experience-grid-subitem" style={{ textAlign: "right" }}>
+        <p>
+          {props.startDate} - {props.endDate}
+        </p>
+      </div>
+      <div className="experience-grid-subitem">
+        <h3>{props.job}</h3>
+        {props.accomplishments}
+      </div>
     </div>
   );
 };
