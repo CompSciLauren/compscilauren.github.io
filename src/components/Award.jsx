06b8ee0284f9a89803f55e74947720435f5c471f
Lauren Stephenson <CompSciLauren@gmail.com>
2020-05-28 23:19:07 -0500

    Fix awards design

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Award = (props) => {
   return (
-    <div class="award-grid-item">
-      <h2>{props.title}</h2>
-      <p>
-        {props.organization}, {props.year}
-      </p>
+    <div className="award-grid-item">
+      <div className="award-grid-item-text">
+        <h2>{props.title}</h2>
+        <p>
+          {props.organization}, {props.year}
+        </p>
+      </div>
     </div>
   );
 };
