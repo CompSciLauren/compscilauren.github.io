c2bf776f8bce6d961efb59615fe5346bc5b8f3d2
Lauren Stephenson <CompSciLauren@gmail.com>
2020-06-14 21:19:36 -0500

    Fix blockquote

--- src/blog/articles/2020-june/Article0615.jsx
+++ src/blog/articles/2020-june/Article0615.jsx
@@ -45,13 +45,15 @@ function Article0615() {
               section.
             </p>
             <blockquote className="classyStyle">
-              Like many other Version Control Systems, Git has a way to fire off
-              custom scripts when certain important actions occur. There are two
-              groups of these hooks: client-side and server-side. Client-side
-              hooks are triggered by operations such as committing and merging,
-              while server-side hooks run on network operations such as
-              receiving pushed commits. You can use these hooks for all sorts of
-              reasons.
+              <span>
+                Like many other Version Control Systems, Git has a way to fire
+                off custom scripts when certain important actions occur. There
+                are two groups of these hooks: client-side and server-side.
+                Client-side hooks are triggered by operations such as committing
+                and merging, while server-side hooks run on network operations
+                such as receiving pushed commits. You can use these hooks for
+                all sorts of reasons.
+              </span>
             </blockquote>
             <p>
               In this article, we will cover two things.
