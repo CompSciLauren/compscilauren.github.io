7ac3d94a8a09f635306a7449c330eafce108726a
Lauren Stephenson <CompSciLauren@gmail.com>
2020-06-14 22:59:44 -0500

    Update article read time

--- src/blog/articles/2020-june/Article0615.jsx
+++ src/blog/articles/2020-june/Article0615.jsx
@@ -20,7 +20,7 @@ function Article0615() {
           <h1 style={{ padding: "8px" }}>
             Improve your Git workflow and save time with Git hooks
           </h1>
-          <BlogProfileTop date="June 15th, 2020" readTime="11 min" />
+          <BlogProfileTop date="June 15th, 2020" readTime="7 min" />
           <div className="tag-container">
             <Tag title="git" />
             <Tag title="automation" />
